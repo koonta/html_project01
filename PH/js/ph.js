d16f29e1a9de4d42fc43f2ac2477dc65a0993070
Koonta <fall_jkc@naver.com>
2019-04-03 17:32:47 +0900

    vcb

--- PH/js/ph.js
+++ PH/js/ph.js
@@ -5,13 +5,22 @@ $(document).ready(function(){
     $(".mbox:nth-child(3)").css({"top":"160px","opacity":"1"});
     $(".mbox:nth-child(4)").css({"top":"240px","opacity":"1"});
     
-    $("div.mbox").click(function(){
+    $(".smwrap ul li").click(function(){
+        var d = $(this).index();
+        $(".smwrap ul li").removeClass();
+        $(".smwrap ul li").eq(d).addClass("long")
         var i = $(this).index();
         console.log(i);
-        $("div.pbox").eq(i+1).offset().top;
+        $("div.pbox").eq(i).offset().top;
         console.log("aaa");
-        var mTop = $("div.pbox").eq(i+1).offset().top;
-       $("html, body").stop().animate({"scrollTop":mTop+"px"},1000); 
+        var mTop = $("div.pbox").eq(i).offset().top;
+       $("html, body").stop().animate({"scrollTop":mTop+"px"},1000);
+        
+    });
+    $(".mbox").click(function(){
+        var d = $(this).index()+1;
+        $(".smwrap ul li").removeClass();
+        $(".smwrap ul li").eq(d).addClass("long");    
     });
     
     
@@ -23,24 +32,26 @@ $(document).ready(function(){
 					if($(this).hasClass("active")){
 						
 						$(this).removeClass("active");
-				    $(this).find(".mbox2").slideUp();	
-						
+				   
 					}
 				});
 			 
 			  $(this).addClass("active");
-			  $(this).find(".mbox2").slideDown();
+			  
 				}else{
-					
+					console.log("sdfawefawe");
 				$(this).removeClass("active");
-				$(this).find(".mbox2").slideUp();	
+				
+                $(".smwrap ul li").removeClass();
+                $(".smwrap ul li").eq(0).addClass("long");    
 				}
 	 });
     
-     $(".pbox").each(function(){
+     $(".pbox").each(function(index){
             $(this).on("mousewheel",function(e){
 //                e.preventDefault();
                 var delta = 0;
+                var lk = window.pageYOffset;
                 delta = event.wheelDelta/120;
                 var moveTop = null; //각페이지의 탑값계산
                 if(delta<0){
@@ -52,13 +63,23 @@ $(document).ready(function(){
                       moveTop=$(this).prev().offset().top;
                       }
                    }
-//               
+                
+//                var kk = $(this).offset().top;
+//                console.log("aa:"+kk+"index:"+index);
+////                var lk = window.pageYOffset;
+//                if(lk>=kk){
+//                    $(".smwrap ul li").removeClass();
+//                    $(".smwrap ul li").eq(index).addClass("long");
+//                }
+                
+
                 $("html, body").stop().animate({"scrollTop":moveTop+"px"},1000);
             });
         });
     
-    
-    
+        
+        
+        
     
     
 });
